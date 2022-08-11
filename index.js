5f3c0dc95b5ea08221cc11f517f1a864b7cad675
cristeacrs <cristea.cristi@yahoo.com>
2022-08-11 12:37:46 +0300

    [FEAT] Update logo/map

---
+++ index.js
@@ -0,0 +1,19 @@
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+
+  const uluru = { lat: 45.2328172, lng: 27.8062892 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: uluru,
+  });
+  mapType: "satellite";
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
